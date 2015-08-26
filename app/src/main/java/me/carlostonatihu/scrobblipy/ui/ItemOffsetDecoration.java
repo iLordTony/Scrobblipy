fb594130c94a5614c7bb02c5a1c97adccf9ce9a7
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-08-26 15:13:37 -0500

    Terminado la logica del home

---
+++ app/src/main/java/me/carlostonatihu/scrobblipy/ui/ItemOffsetDecoration.java
@@ -0,0 +1,8 @@
+package me.carlostonatihu.scrobblipy.ui;
+
+/**
+ * Project: Scrobblipy
+ * Created by Carlos Tonatihu Barrera on 26/08/2015.
+ */
+public class ItemOffsetDecoration {
+}
--- app/src/main/java/me/carlostonatihu/scrobblipy/ui/fragment/HypedArtistsFragment.java
+++ app/src/main/java/me/carlostonatihu/scrobblipy/ui/fragment/HypedArtistsFragment.java
@@ -9,19 +9,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import me.carlostonatihu.scrobblipy.R;
+import me.carlostonatihu.scrobblipy.domain.Artist;
+import me.carlostonatihu.scrobblipy.ui.adapter.HypedArtistsAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HypedArtistsFragment extends Fragment {
+
     private RecyclerView mRecyclerHypedArtists;
     private static final int NUM_COLLUMS = 2;
+    private HypedArtistsAdapter adapter;
 
     public HypedArtistsFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new HypedArtistsAdapter(getActivity());
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,11 +41,23 @@ public class HypedArtistsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_hyped_artists, container, false);
         mRecyclerHypedArtists = (RecyclerView) view.findViewById(R.id.recycler_hyped_artists);
         setupRecyclerArtists();
+        setContent();
         return view;
     }
 
     private void setupRecyclerArtists () {
         mRecyclerHypedArtists.setLayoutManager(new GridLayoutManager(getActivity(), NUM_COLLUMS));
+        mRecyclerHypedArtists.setAdapter(adapter);
+    }
+
+    private void setContent() {
+        ArrayList<Artist> artists = new ArrayList<>();
+
+        for (int i= 0; i < 10; i++) {
+            artists.add(new Artist("Artist " + 1));
+        }
+
+        adapter.addAll(artists);
     }
 
 }
